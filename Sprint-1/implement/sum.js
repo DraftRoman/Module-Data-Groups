906b16df04df84da29a8a9460aa973b5092eb22c
Roman <roman.boxes@gmail.com>
2026-01-05 17:14:01 +0100

    type validation / wrote tests
    - added if statement for empty array
    - type validation
    - return null if don't have any number on the array

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,9 +1,15 @@
 function sum(elements) {
     let sum = 0;
+    if (elements.length === 0)
+        return sum;
+    let foundNumber = false;
     for (let i = 0; i < elements.length; i++) {
-        sum += elements[i];
+        if (typeof elements[i] === 'number') {
+            sum += elements[i];
+            foundNumber = true;
+        }
     }
-    return sum;
+    return foundNumber ? sum : null;
 }
 
 console.log(sum([]));
