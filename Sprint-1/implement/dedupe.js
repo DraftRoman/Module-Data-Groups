c65213205dd1757ec1f50e58c7f6b114a0ae1d36
Roman <roman.boxes@gmail.com>
2026-01-06 12:15:57 +0100

    Implementation of dedupe function
    - the first version of dedupe function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,12 @@
-function dedupe() {}
+function dedupe(array) {
+    for (let i = 0; i < array.length-1; i++) {
+        for (let j = 1; j < array.length; j++) {
+            if (array[i] === array[j] && i != j) {
+                array.splice(j, 1);
+            }
+            
+        }
+    }
+    return array;
+}
+console.log(dedupe(['a', 'b', 'a', 5, 5]));
