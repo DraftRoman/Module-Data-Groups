ec15b37b96f8b8bc8ba468e6a7f43116d909be82
Roman <roman.boxes@gmail.com>
2026-01-12 21:45:30 +0100

    fix(recipe) recipe
    The access to the object's value should be recipe.ingredients not just recipe is not enough.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// The last part of the statement in the console.log is incorrect because it should be recipe.ingredients
+// This way it will try to print the whole object.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -12,4 +14,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+  ${recipe.ingredients}`);  
