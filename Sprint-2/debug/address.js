eaddf2b0a6439546d06e1ac9f61d1733a41fab48
Roman <roman.boxes@gmail.com>
2026-01-12 17:46:02 +0100

    fix (property access)
     - address.houseNumber is the correct way to get property from the object
    - practice in basic syntax with objects

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -11,5 +11,31 @@ const address = {
   country: "England",
   postcode: "XYZ 123",
 };
+// we have to use address.anyProperty to access to that property, and not just number.
+console.log(`My house number is ${address.houseNumber} on ${address.street} in ${address.city}, ${address.postcode}, ${address.country}.`);
 
-console.log(`My house number is ${address[0]}`);
+// const profileData = {
+//   firstName: "Francesco",
+//   lastName: "Leoni",
+//   age: 33,
+// };
+
+
+
+// const paragraph = "I think Ruth's dog is cuter than your dog!";
+
+// console.log(paragraph.replace("Ruth's", "my"));
+// // Expected output: "I think my dog is cuter than your dog!"
+
+// const regex = /dog/i;
+// console.log(paragraph.replace(regex, "ferret"));
+// // Expected output: "I think Ruth's ferret is cuter than your dog!"
+// let query = "colour=blue&sort=newest";
+// const obj = query
+//   .split("&")
+//   .map(pair => pair.split("=")).reduce((acc, [key, value]) => {
+//     acc[key] = value;
+//     return acc;
+//   }, {});
+
+// console.log(obj);
