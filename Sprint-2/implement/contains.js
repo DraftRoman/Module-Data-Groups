e221a0b3f515f600cee0a29f3949d33fff493602
Roman <roman.boxes@gmail.com>
2026-01-15 11:04:10 +0100

    Implement the function and basic tests
    Function:
    - checking type if it's object and if it's null because null in Javascript is an object.
    - used hasOwnProperty method. The MDN recommended to use hasOwn method, but it isn't work for me.
    Tests:
    - "contains returns true for existing property" passed
    - "contains returns false for non-existing property" passed
    - "contains returns false for non-object input", passed
    - "contains on empty object returns false" passed
    - "Given invalid parameters like an array" passed ( even of we're going to have a same property, but this method is not experiencing array as a property, only strings and symbols.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
-
+function contains(object, property) {
+    if (typeof object !== 'object' || object === null) {
+        return false;
+    }
+    return object.hasOwnProperty(property);
+}
+const object = [2, 56, 'a', 'b', [], null];;
+console.log(contains(object, []));
 module.exports = contains;
