112dcc2f2ce82d468e094a8cc0c639840c9e42e9
Roman <roman.boxes@gmail.com>
2026-01-19 22:03:11 +0100

    interpret - invert / implemented tests
    Fixed the function:
    - invalid variable reference
    - the value in this case acts as a key, and has to be on a position of the key.
    - moved all questions to the tests, because in this file we can see actual results.
    - tested the results of the questions.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,11 +10,12 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
+module.exports = invert;
 
 // a) What is the current return value when invert is called with { a : 1 }
 
