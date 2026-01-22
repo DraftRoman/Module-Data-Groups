54ebf1531d59556cf12ac71195b9424854e957d0
Roman <roman.boxes@gmail.com>
2026-01-22 17:52:50 +0100

    formatTime / message / setAlarm / countdown / set btn/ stop btn
    HTML  added the rule for input. It has to be more than 0.
    JS
    - setAlarm() takes input from the user and updates timeRemaining to set the alarm.
    - formatTime takes seconds as an input, it converts to minutes and seconds in format 00:00.
    - function message(time) just to not repeat ourselves and make the code shorter.
    - function countdown(alarmTime) countdowns, has a 1sec interval between updates "timeRemaining". When alarmTime = 0, play the alarm and stop.
    - The set button starts the setAlarm and countdown functions
    - The stop button stops the alarm, cleans the user input, and  Time Remaining.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,16 +1,41 @@
-function setAlarm() {}
+function setAlarm(alarmTime) {
+    document.getElementById('timeRemaining').innerHTML = message(alarmTime);
+}
+function formatTime(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    return `${min}:${sec < 10 ? "0" : ""}${sec}`;
+  }
+  function message(time) {
+    return "Time Remaining: " + formatTime(time);
+  }
+  
 
-// DO NOT EDIT BELOW HERE
+  function countdown(alarmTime) {
+    let interval = setInterval(() => {
+      alarmTime--;
+      document.getElementById('timeRemaining').innerHTML = message(alarmTime);
+      if (alarmTime == 0) {
+        clearInterval(interval);
+        playAlarm();
+      }
+    }, 1000);
+  }
+    
+  // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
+    setAlarm(document.getElementById('alarmSet').value);
+    countdown(document.getElementById('alarmSet').value);
   });
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
+    document.getElementById('alarmSet').value = 0;
+    document.getElementById('timeRemaining').innerHTML = "Time Remaining: 0:00";
   });
 }
 
