f7b6f5f89d2bef6f247c12c4969c8fd2147c2740
Roman <roman.boxes@gmail.com>
2026-01-24 22:45:15 +0100

    newQuote function / split the data and code
    - split the data (file quotes.js) and code (code.js)
    HTML
    - added <script type="module" src="code.js"></script> line
    - used exports and imports
    -the function new Quote passes the quote and the author  to the DOM every time button is clicked

---
+++ Sprint-3/quote-generator/code.js
@@ -0,0 +1,7 @@
+import { quotes, pickFromArray } from './quotes.js';
+function newQuote() {
+    const quoteObj = pickFromArray(quotes);
+    document.getElementById('quote').textContent = quoteObj.quote;
+    document.getElementById('author').textContent = quoteObj.author;
+}
+document.getElementById("new-quote").addEventListener("click", newQuote);
